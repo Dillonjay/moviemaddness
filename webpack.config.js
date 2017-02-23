3f74bacfdcf8a4b5efd6dc95d7060e1e1946dde5
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-23 09:18:32 -0600

    set up basic webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTemplate = require('html-webpack-template');
+
+
+const paths = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+	devServer: {
+		inline: true,
+		port: process.env.PORT,
+		contentBase: paths.app
+	},
+	entry: paths.app,
+	output: {
+		path: paths.build,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test:/\.jsx?/,
+				loader: 'babel-loader',
+				include: paths.app
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: HtmlWebpackTemplate,
+			title: 'Movie Maddness',
+			appMounted: 'app',
+			mobile: true
+		})
+	]
+};
