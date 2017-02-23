7f3ac3329d36e99fcbc01e638e49b3d1fec6cf2b
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-23 10:10:17 -0600

    set up basic component for adding a movie

---
+++ app/components/add_movie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+export default class AddMovie extends from React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+
+		}
+	}
+}
--- app/components/main.js
+++ app/components/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Movies from './movies.js';
+import AddMovie from './add_movie.js';
 
 export default class Main extends React.Component {
 	render() {
@@ -16,6 +17,7 @@ export default class Main extends React.Component {
 				<div>
 					<h1>You currently have no movies to display.</h1>
 					<h1>Want to add one ?</h1>
+					<AddMovie movies={this.props.movies} />
 				</div>
 			}
 			</div>
