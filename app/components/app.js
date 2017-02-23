317c60559f110c5aaf15ca0298d0e27d4890fae5
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-23 12:26:50 -0600

    Explain how dummy data works

--- app/components/app.js
+++ app/components/app.js
@@ -3,13 +3,15 @@ import Main from './main.js';
 import Dummy from '../dummy_data.js';
 import Header from './header.js';
 
+// To use pre made movie data, comment out lines 8 & 9.
+// Then uncomment lines 12 & 13.
+
 if(!localStorage.hasOwnProperty('movies')) localStorage.setItem('movies', JSON.stringify( [] ));
 const Movies = JSON.parse( localStorage.getItem('movies') );
 
+// if(!localStorage.hasOwnProperty('movies')) localStorage.setItem('movies', JSON.stringify( Dummy ));
+// const Movies = JSON.parse( localStorage.getItem('movies') );
 
-// To pre populate movie list, swap out 
-// <Main movies={ Movies }/> with 
-// <Main movies={ Dummy }
 
 const App = () => (
 	<div>
--- app/components/main.js
+++ app/components/main.js
@@ -6,7 +6,6 @@ import AddMovie from './add_movie.js';
 
 export default class Main extends React.Component {
 	render() {
-		console.log('this.props.movies', this.props.movies)
 		return (
 			<div>
 			{ 
