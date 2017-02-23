45392b21728cdff45a05597494b5780b1063c7f2
Dillon Lundell <dillonjaylundell@gmail.com>
2017-02-23 09:52:46 -0600

    create a dummy data file for testing movie display

---
+++ app/dummy_data.js
@@ -0,0 +1,65 @@
+const dummyData = [
+	{
+		title: 'The ring',
+		genre: 'horror',
+		actors: [ 'dillon', 'tim'],
+		year: 1999,
+		rating: 5
+	},
+	{
+		title: 'Sinister',
+		genre: 'horror',
+		actors: [ 'John', 'Brad Pitt'],
+		year: 2014,
+		rating: 5
+	},
+	{
+		title: 'The Note Book',
+		genre: 'romance',
+		actors: [ 'Ryan', 'Rachel'],
+		year: 1990,
+		rating: 2
+	},
+	{
+		title: 'The Last Of the Mohicans',
+		genre: 'action',
+		actors: [ 'Daniel', 'Jimmy'],
+		year: 1983,
+		rating: 5
+	},
+	{
+		title: 'Spider',
+		genre: 'action',
+		actors: [ 'Jhonny', 'Jill'],
+		year: 2014,
+		rating: 5
+	},
+	{
+		title: 'Iron Man',
+		genre: 'action',
+		actors: [ 'Robert', 'Kelly'],
+		year: 2013,
+		rating: 3
+	},
+	{
+		title: 'Rings',
+		genre: 'horror',
+		actors: [ 'Bill', 'Rae'],
+		year: 2017,
+		rating: 4
+	},
+	{
+		title: 'Saving Private Ryan',
+		genre: 'action',
+		actors: [ 'Hank', 'Matt'],
+		year: 1995,
+		rating: 5
+	},
+	{
+		title: 'The Martian',
+		genre: 'action',
+		actors: [ 'Matt', 'tim'],
+		year: 2013,
+		rating: 5
+	}
+]
